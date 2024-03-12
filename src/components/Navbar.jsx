2294a06d81abb301e8854de688ec4a09212040ac
Animesh Mondal <animeshmondal1414@gmail.com>
2024-03-12 10:53:25 +0530

    added mobile menu and title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
+
 function Navbar() {
+  const [menu, setMenu] = useState(false);
+
   return (
-    <nav className="lg:px-10 px-8 pt-5 lg:pb-20 pb-32 flex items-center justify-between">
+    <nav className="relative lg:px-10 px-8 pt-5 lg:pb-20 pb-32 flex items-center justify-between">
       <h1 className="text-white font-bold text-3xl cursor-pointer font-[Barlow]">
         sunnyside
       </h1>
@@ -29,11 +33,24 @@ function Navbar() {
       </div>
       <div className="lg:hidden">
         <img
-          className="cursor-pointer"
+          className={`cursor-pointer ${menu ? "opacity-50 " : ""}`}
           src="/images/icon-hamburger.svg"
           alt="humburger"
+          onClick={() => setMenu(!menu)}
         />
       </div>
+      {menu && (
+        <div className="bg-white pt-10 pb-20 px-16 lg:hidden flex flex-col gap-5 justify-center items-center absolute -bottom-52 right-10">
+          <ul className="flex flex-col justify-center items-center gap-5 font-[Barlow]">
+            <li className="font-semibold text-slate-600">About</li>
+            <li className="font-semibold text-slate-600">Services</li>
+            <li className="font-semibold text-slate-600">Projects</li>
+          </ul>
+          <button className="py-5 px-12 rounded-full bg-yellow-400 font-[Fraunces] font-bold uppercase">
+            Contact
+          </button>
+        </div>
+      )}
     </nav>
   );
 }
